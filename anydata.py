7f1619b1ba293371179a55331f88e23396495c66
qbt29 <zubakov.yaroslaw@yandex.ru>
2025-09-16 16:19:49 +0700

    Add comments

--- anydata.py
+++ anydata.py
@@ -3,24 +3,38 @@ import argparse
 from PIL import Image, ImageDraw
 
 def read_data(path):
+    '''
+    Чтение данных из фото
+    :param path:
+    :return:
+    '''
     try:
         image = cv2.imread(path)
         gimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return image, gimage
     except:
         raise Exception("Incorrect path to file")
-        exit(0)
 
+def find_corners(gimage):
+    '''
+    Находит углы в gimage
+    :param gimage:
+    :return:
+    '''
+    corners = cv2.goodFeaturesToTrack(gimage, maxCorners=1000, qualityLevel=0.08, minDistance=10, useHarrisDetector=False)
+    return corners
 def main(path):
     image, gimage = read_data(path)
-    corners = cv2.goodFeaturesToTrack(gimage, maxCorners=1000, qualityLevel=0.08, minDistance=10, useHarrisDetector=False)
+    corners = find_corners(gimage)
 
     cv2.imwrite('found.jpg', image)
     im = Image.open('found.jpg')
     draw = ImageDraw.Draw(im)
+    # Добавляем круги вокруг найденных углов
     for corner in corners:
         x,y = map(int, corner[0])
         draw.ellipse((x-10,y-10, x+10, y+10), outline="red")
+    # Сохраняем изображение в found.jpg
     im.save("found.jpg", quality=120)
     im.close()
 
--- main.py
+++ main.py
@@ -3,19 +3,27 @@ import matplotlib.patches as patches
 import cv2
 from skimage import data
 
-def main():
-
-    gimage = data.coins()
+def find_corners(gimage):
+    '''
+    Находит углы в gimage
+    :param gimage:
+    :return:
+    '''
     corners = cv2.goodFeaturesToTrack(gimage, maxCorners=25, qualityLevel=0.01, minDistance=10)
+    return corners
 
+def main():
+    gimage = data.coins() #Загружаем изображение
+    corners = find_corners(gimage)
     plt.plot()
     ax = plt.gca()
     ax.imshow(gimage)
-
+    # Добавляем круги вокруг найденных углов
     for corner in corners:
         x,y = map(int, corner[0])
         ax.add_patch(patches.Circle((x, y), 10, linewidth=2, edgecolor="red", fill=None))
 
+    # Визуализация
     plt.show()
 
 if __name__=='__main__':
